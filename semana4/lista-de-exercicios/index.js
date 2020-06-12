7cd5acc99ec622c13ad1f073d777ecbcbb825aa3
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-06-12 12:51:19 -0300

    Exercicio 1 (Interpretação de Código)

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,4 @@
+//Exercicios de Interpretação de código
+
+/* Respostas
+1- Nesse exercicio a função conversor de moeda recebe o parâmetro valorEmDolar. A cotação será definida pelo usuário atrávez do prompt. Irá retornar o valor do parâmetro da função multiplicado pela a cotação do dólar. No caso será impresso no console, o valor da constante meuDinheiro que será o resultado dessa função tendo como valorEmDolar 100, que será multiplicado pela cotação que o usuário digitar.

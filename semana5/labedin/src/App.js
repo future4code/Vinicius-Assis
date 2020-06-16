0fd0843b6c0dbe1ba49ae1daf09cc8365f412ec1
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-06-16 17:01:41 -0300

    Exercicio completo de vdd kkk

--- semana5/labedin/src/App.js
+++ semana5/labedin/src/App.js
@@ -36,13 +36,13 @@ function App() {
         <CardGrande 
           imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
           nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          descricao="Estudei e me tornei dev Web Full-Stack. E fui contratado após o curso." 
         />
         
         <CardGrande 
           imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
           nome="NASA" 
-          descricao="Apontando defeitos." 
+          descricao="Trabalhando desde minha formação na Labenu" 
         />
       </div>
 

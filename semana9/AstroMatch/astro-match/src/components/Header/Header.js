f82b424f200d2f771d201add6f7b152b4a7cb2d2
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-07-16 18:05:07 -0300

    Tomando forma

--- semana9/AstroMatch/astro-match/src/components/Header/Header.js
+++ semana9/AstroMatch/astro-match/src/components/Header/Header.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import styled from "styled-components"
+
 const Cabecalho = styled.header`
 background-color:;
 grid-row: 1/1;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+const H2 = styled.h2`
+display: inline;
+background: -webkit-linear-gradient(#00ced1, #333);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+const H22 = styled.h2`
+display: inline;
+background: -webkit-linear-gradient(red, black);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`
+
+const Button = styled.div`
+display: flex;
+justify-content: center;
+width: 60px;
+height: 60px;
+border-radius: 100%;
+background: -webkit-linear-gradient(#9b111e, black, #9b111e);
+`
+const P = styled.p`
+display: flex;
+align-items: center;
+color: white;
+font-weight: bold;
 `
 
 
 function Header(props) {
     return (
       <Cabecalho>
-       <button onClick={props.a}>Matchs</button>
+        <H2>astro</H2>
+        <H22>match</H22>
+        <Button onClick={props.a}>
+            <P>Matchs</P>
+        </Button>
       </Cabecalho>
     );
   }

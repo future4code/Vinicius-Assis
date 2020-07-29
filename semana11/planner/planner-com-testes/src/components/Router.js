80f0bb5b0cd2fdf6d17d48823a420cdd0f9ad54d
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-07-29 17:19:14 -0300

    Iniciando

---
+++ semana11/planner/planner-com-testes/src/components/Router.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react"
+//import {Switch, Route, BrowserRouter} from "react-router-dom"
+
+function Router() {
+    return (
+      <div>
+          aaa
+      </div>
+    );
+  }
+  
+  export default Router;
